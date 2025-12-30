d08e7f5119321d8fd3f7a770538fedbcc4e93767
izeroo <22554102+izeroo@users.noreply.github.com>
2025-12-30 12:25:40 +0800

    Update build_tiny_linux.sh

--- build_tiny_linux.sh
+++ build_tiny_linux.sh
@@ -1019,7 +1019,7 @@ build_newroot()
     # Prepare locales
     [[ -d "$NEWROOT"/usr/lib/locale ]] && die "Unexpected directory: $NEWROOT/usr/lib/locale"
     [[ -d "$NEWROOT"/usr/lib64/locale ]] && die "Unexpected directory: $NEWROOT/usr/lib64/locale"
-    locale-gen -c "$BUILDSCRIPTS/locale.gen" -d "$NEWROOT"
+    # locale-gen -c "$BUILDSCRIPTS/locale.gen" -d "$NEWROOT"
     if [[ -d "$NEWROOT"/usr/lib && -d "$NEWROOT"/usr/lib64 && -d "$NEWROOT"/usr/lib/locale && ! -d "$NEWROOT"/usr/lib64/locale ]]; then
         mv "$NEWROOT"/usr/lib/locale "$NEWROOT"/usr/lib64/locale
     fi
